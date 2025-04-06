faff50a651a702ce71b369e348277f2e8a4d5da7
confjuzen <toby@confjuzen.com>
2025-04-06 02:53:31 +0200

    🦄

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './style.css'
 import Home from './pages/Home.tsx'
+import Three from './components/three.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
+    <>
+      <Home />
+
+      <Three />
+    </>
   </StrictMode>,
 )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,20 @@
 
-function Home() {
+const Home = () => {
     return (
-        <>
-            <h1>
-                TOBY FOSTER
-            </h1>
-        </>
-  )
-}
+        <div className="border grid">
+            <div className="item title1"><h1>· TOBY FOSTER ·</h1></div>
+            <a className="button fill item link1" href="mailto:toby@confjuzen.com">EMAIL</a>
+            <a className="button fill item link2" href="https://www.github.com/confjuzen/">GITHUB</a>
+            <a className="button fill item link3" href="https://glitch.com/@confjuzen">GLITCH</a>
+            <a className="button fill item link4" href="https://www.instagram.com/confjuzen/">INSTAGRAM</a>
+            <div className="item title2"><h2>PROJECTS</h2></div>
+            <a className="button fill item pro1" href="https://confjuzen.github.io/booki/">BOOKI</a>
+            <a className="button fill item pro2" href="https://confjuzen.github.io/Portfolio-architecte-sophie-bluel">PORTFOLIO DYNAMIC</a>
+            <a className="button fill item pro3" href="https://toby-foster.blogspot.com">MY GRAPHIC DESIGN</a>
+            <div className="item decription"></div>
+        <div className="item2 progap"></div>
+     </div>
+    )
+};
 
-export default Home
+export default Home;
