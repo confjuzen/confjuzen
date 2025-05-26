39965b086a8907224deed93aea753a8bb3475bc6
confjuzen <toby@confjuzen.com>
2025-05-26 18:42:28 +0200

    test mobile 2

--- src/pages/Webdev.tsx
+++ src/pages/Webdev.tsx
@@ -1,53 +1,110 @@
 //import { useState } from "react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Webdev: React.FC = () => {
-  const { t } = useTranslation('webprojects'); 
-  const projects = Object.values(t('webprojects', { returnObjects: true })); // Get the projects as an array
+  const { t } = useTranslation("webprojects");
+  const projects = Object.values(t("webprojects", { returnObjects: true })); // Get the projects as an array
 
-//  const [isOpen, setIsOpen] = useState(false);
-//  const [locked, setLocked] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [locked, setLocked] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
+  const [ tempIndex, setTempIndex ] = useState<number | null>(null);
 
-//  function handleMouseEnter() {
-//    if (!locked) {
-//      setIsOpen(true);
-//    }
-//  }
-//
-//  function handleMouseLeave() {
-//    if (!locked) {
-//      setIsOpen(false);
-//    }
-//  }
-//
-//// / function handleClick() {
-/// /   if (isOpen && !locked) {
-///      setLocked(true);
-///    } else if (isOpen && locked) {
-///      setLocked(false);
-///      setIsOpen(false);
-///    }
-///  }
+  function handleMouseEnter(index: number) {
+    if (!locked) {
+      setIsOpen(true);
+      setSelectedIndex(index);
+    }
+    if (locked) {
+      setTempIndex(selectedIndex);
+      setSelectedIndex(index);
+    }
+  }
+
+  function handleMouseLeave() {
+    if (!locked) {
+      setIsOpen(false);
+      setSelectedIndex(null);
+    }
+    if (locked) {
+      setSelectedIndex(tempIndex);
+    }
+  }
+
+  function handleClick(index: number) {
+    if (isOpen && !locked) {
+      setLocked(true);
+      setTempIndex(index)
+      setSelectedIndex(index);
+    } else if (isOpen && locked) {
+      setSelectedIndex(index);
+      setTempIndex(index);
+    } else if (isOpen && locked && selectedIndex == index) {
+      setLocked(false);
+      setSelectedIndex(null);
+      setTempIndex(null);
+    } 
+  }
+
+
+
 
   return (
     <>
-
-
       <div className={"webProjects"}>
         {projects.map((project, index) => (
-          <Link
+          <h3
             key={index}
-            className={`fill item link ${'drop' + index}`}
-            to={project.url}
+            className={`fill item link ${"drop" + index} ${
+              selectedIndex === index ? "open" : ""
+            }`}
             rel="noopener noreferrer"
-     //       onMouseLeave={handleMouseLeave}
-     //       onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+            onMouseEnter={()=>handleMouseEnter(index)}
+            onClick={()=>handleClick(index)}
           >
             {project.title}
-          </Link>
+          </h3>
         ))}
       </div>
+
+      {selectedIndex !== null && (
+        <>
+      <div className={`img1 item ${isOpen ? "grow" : "shrink"}`}>
+          <img
+            className={"img1i"}
+            src={projects[selectedIndex].img1}
+            alt={projects[selectedIndex].title}
+          />
+        
+      </div>
+      <div className={`img2 item ${isOpen ? "grow" : "shrink"}`}>
+          <img
+            className={"img2i"}
+            src={projects[selectedIndex].img2}
+            alt={projects[selectedIndex].title}
+          />
+        
+      </div>
+      <div className={`img3 item ${isOpen ? "grow" : "shrink"}`}>
+          <img
+            className={"img3i"}
+            src={projects[selectedIndex].img3}
+            alt={projects[selectedIndex].title}
+          />
+      </div>
+      <div className="tagsline">
+        {projects[selectedIndex].tags.map((tag: string, index: number) => (
+          <div key={index} className="tag item">
+            {tag}
+          </div>
+        ))}
+
+      </div>
+      </>
+      )}
     </>
   );
 };
